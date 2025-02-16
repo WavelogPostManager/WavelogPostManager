ef3bcfcecc2e52cf3115bf432f690f2878495bf1
NearlyHeadlessJack <wang@rjack.cn>
2025-02-16 14:47:20 +0800

    v0.0.1b4

--- wavelogpostmanager/constants/core_constant.py
+++ wavelogpostmanager/constants/core_constant.py
@@ -16,15 +16,15 @@ PACKAGE_NAME = "wavelogpostmanager"
 CLI_COMMAND = NAME_SHORT
 
 # WavelogPostManager Version Storage
-VERSION_PYPI: str = "0.0.1b3"
-VERSION: str = "0.0.1-beta3"
+VERSION_PYPI: str = "0.0.1b4"
+VERSION: str = "0.0.1-beta4"
 
 API_VERSION = 1.0
 
 
 # URLs
 GITHUB_URL = r"https://github.com/WavelogPostManager/WavelogPostManager"
-# GITEE_VERSION_URL = r"https://gitee.com/NHJ2001/WeAuth/raw/main/VERSION"
+
 DOCUMENTATION_URL = (
     r"https://github.com/WavelogPostManager/WavelogPostManager/blob/main/README.md"
 )
